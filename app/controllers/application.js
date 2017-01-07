42d4fb5615589be3e77842a15acc09985c42a07b
Alex Bachuk <alex@bachuk.com>
2017-01-07 00:55:58 -0500

    Global application container, refactor header

---
+++ app/controllers/application.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  currentPathDidChange: function() {
+    this.set('homeHeader', this.currentPath === 'index' ? true : false);
+  }.observes('currentPath'),
+});
