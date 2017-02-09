ab9afc0b574e434c05513a607af0b385d4e2104e
Alex Bachuk <alex@bachuk.com>
2017-02-08 23:10:32 -0500

    services page

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,5 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   currentPathDidChange: function() {
     this.set('homeHeader', this.currentPath === 'index' ? true : false);
+    this.set('route', this.currentPath);
   }.observes('currentPath'),
 });
